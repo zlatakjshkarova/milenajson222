fbfb40eda840c0996e471789ef14f74b904d16cb
zlatakjshkarova <147971125+zlatakjshkarova@users.noreply.github.com>
2025-04-12 14:52:29 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,46 +2,41 @@
 let json5 = ["json1", "json2", "json3", "json4", "json5", "json6"]
 let dfg = JSON.stringify(json5)
 console.log(dfg)
-//5
-let ff = `[
-    {
-        "surname": "Иванов",
-        "name": "Иван",
-        "patronymic": "Иванович"
-
-    },
 
 
-    {
-        "surmane": "Петров",
-
-        "name": "Петров",
-        "patronymic": "Петрович"
-    },
-
-    {
-        "surname": "Сидоров",
-        "name": "Сидор",
-        "patronymic": "Сидорович"
-
-
-    }
-]`
-
-console.log(JSON.parse(ff))
-
-
-
-
-let json = `['user1', 'user2', 'users', 'user4', 'user5']`;
+let json = ['user1', 'user2', 'user3', 'user4', 'user5'];
 const jsonScript = JSON.stringify(json)
-let users = ['user1', 'user2', ' user3', 'user4', 'users5']
 
-let lists = Array.from(document.querySelectorAll(" .list li"));
+let users = ['user1', 'user2', 'user3', 'user4', 'user5']
+//получить колекцию списков элементов и преобразовать ее в массив(Array.from)
+let lists = Array.from(document.querySelectorAll(".list li"));
+
 console.log(lists)
+//перебрать все элементы списка и вернуть их текстовое содержание
 const item = lists.map((list) => {
     return list.textContent
 })
+console.log(item)
+
+
+const user = [{
+
+    "name": "user1",
+    "age": 25,
+    "salary": 1000
+},
+{
+    "name": "user2",
+    "age": 26,
+    "salary": 2000
+},
+{
+    "name": "user3",
+    "age": 27,
+    "salary": 3000
+
+}]
+
 //2
 const list = [
     {
@@ -68,31 +63,25 @@ console.log(jsonStringify)
 
 
 
-
-
-
-
-
-
-//4 
+//4
 let json2 = `[
-{
-"name":"user1",
-"age":25,
-"salary":1000
-},
-{
-"name":"user2",
-"age":26,
-"salary":2000
-},
-{
-"name":"user3",
-"age":27,
-"salary":3000
-}
-]`;
-
+    {
+    "name": "user1",
+    "age": 25,
+    "salary": 1000
+    },
+    {
+    "name": "user2",
+    "age": 26,
+    "salary": 2000
+    },
+    {
+    "name": "user3",
+    "age": 27,
+    "salary": 3000
+    }
+    ]`;
+//perse обратно
 const employees = JSON.parse(json2);
 const tbody = document.querySelector('#employesBody');
 
@@ -100,9 +89,52 @@ for (let employee of employees) {
     let tr = document.createElement('tr')
     tbody.appendChild(tr);
     tr.innerHTML = `
-<td>${employee.name}</td>
-<td>${employee.age}</td>
-<td>${employee.salary}</td>
-`
+    <td>${employee.name}</td>
+      <td>${employee.age}</td>
+        <td>${employee.salary}</td>
+    `
 }
+
+
+//5
+
+
+
+/* Получите представленные данные в формате JSON в виде объекта с
+ключами surname, name, patronymic. */
+
+
+/* //найти все строки таблицы кроме 1 */
+const rows = document.querySelectorAll('#task5 tr:not(:first-child)');
+
+/* создать пустой масив для результатов */
+const result = [];
+
+
+//перебрать каждую строку в таблице 
+rows.forEach((row) => {
+    //найти все ячейки в каждой строке
+    const cells = row.querySelectorAll('td')
+    //создать обьект для 1 человека
+    const person = {
+        //первая ячейка фамилии
+        surname: cells[0].textContent,
+        //вторая ячейка
+        name: cells[1].textContent,
+        //третья ячейка отчество
+        patronymic: cells[2].textContent
+    }
+
+
+
+    //добавить элементы в массив
+    result.push(person)
+
+})
+//приоброзовать массив в json
+const jsonResult = JSON.stringify(result)
+
+console.log(jsonResult)
+
+
 
